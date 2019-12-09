3cba681b3095f3d1e34aebc8fddd20d3abe64b7c
atsushi totsuka <a.totsuka@makip.co.jp>
2019-12-09 18:52:53 +0900

    init

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -eu
+
+REDASH_BASE_PATH=$(pwd)
+
+create_directories() {
+    if [[ ! -e $REDASH_BASE_PATH ]]; then
+        mkdir -p $REDASH_BASE_PATH
+    fi
+
+    if [[ ! -e $REDASH_BASE_PATH/postgres-data ]]; then
+        mkdir $REDASH_BASE_PATH/postgres-data
+    fi
+}
+
+create_config() {
+    if [[ -e $REDASH_BASE_PATH/env ]]; then
+        rm $REDASH_BASE_PATH/env
+        touch $REDASH_BASE_PATH/env
+    fi
+
+    COOKIE_SECRET=$(pwgen -1s 32)
+    SECRET_KEY=$(pwgen -1s 32)
+    POSTGRES_PASSWORD=$(pwgen -1s 32)
+    REDASH_DATABASE_URL="postgresql://postgres:${POSTGRES_PASSWORD}@postgres/postgres"
+
+    echo "PYTHONUNBUFFERED=0" >> $REDASH_BASE_PATH/env
+    echo "REDASH_LOG_LEVEL=INFO" >> $REDASH_BASE_PATH/env
+    echo "REDASH_REDIS_URL=redis://redis:6379/0" >> $REDASH_BASE_PATH/env
+    echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> $REDASH_BASE_PATH/env
+    echo "REDASH_COOKIE_SECRET=$COOKIE_SECRET" >> $REDASH_BASE_PATH/env
+    echo "REDASH_SECRET_KEY=$SECRET_KEY" >> $REDASH_BASE_PATH/env
+    echo "REDASH_DATABASE_URL=$REDASH_DATABASE_URL" >> $REDASH_BASE_PATH/env
+}
+
+setup_compose() {
+    docker-compose run --rm server create_db
+    docker-compose up -d
+}
+
+create_directories
+create_config
+setup_compose
